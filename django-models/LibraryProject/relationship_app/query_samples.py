05b0879a842a46526f50c0de094638ae5f1494b5
brianbond <tumuhimbise92@gmail.com>
2025-07-20 18:16:24 +0300

    Add relationship models and queres

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -84,7 +84,7 @@ def retrieve_librarian_for_library(library_name):
         library = Library.objects.get(name=library_name)
         
         # Get the librarian using the correct relationship
-        librarian = Librarian.objects.filter(library=library).first()
+        librarian = Librarian.objects.get(library=library)
         if librarian:
             print(f"Librarian for {library_name} Library: {librarian.name}")
             return librarian
