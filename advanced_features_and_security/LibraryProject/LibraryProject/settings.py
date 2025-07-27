c5487b10ad2680029254c45c05cf0dd7afd88f55
brianbond <tumuhimbise92@gmail.com>
2025-07-27 22:04:38 +0300

    advancedfeatures

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -140,3 +140,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# If behind a proxy/load balancer that sets X-Forwarded-Proto, enable this:
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
