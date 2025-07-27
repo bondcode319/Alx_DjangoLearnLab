108527585665d82dbfb21affbd35c557a1a50908
brianbond <tumuhimbise92@gmail.com>
2025-07-27 21:28:10 +0300

    advancedfeatures

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = '__all__'  #
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+
+    def clean_name(self):
+        data = self.cleaned_data['name']
+        if 'admin' in data.lower():
+            raise forms.ValidationError("Name cannot contain 'admin'")
+        return data
