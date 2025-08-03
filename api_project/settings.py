b514014f26db30292bca6c50e6b08bf40a604651
brianbond <tumuhimbise92@gmail.com>
2025-08-03 15:58:09 +0300

    Initial commit

--- api_project/settings.py
+++ api_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  # Django REST Framework
     'api',
-    'rest_framework.authtoken'# Your API application
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
