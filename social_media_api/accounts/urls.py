3fd240ecffb3a06a8a5b3fc462f29362b9b58d43
brianbond <tumuhimbise92@gmail.com>
2025-08-24 23:07:02 +0300

    commit

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, LoginView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -8,6 +8,8 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('register/', UserViewSet.as_view({'post': 'create'}), name='register'),
-    path('login/', UserViewSet.as_view({'post': 'login'}), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('follow/<int:user_id>/', UserViewSet.as_view({'post': 'follow'}), name='follow-user'),
+    path('unfollow/<int:user_id>/', UserViewSet.as_view({'post': 'unfollow'}), name='unfollow-user'),
     path('api-auth/', include('rest_framework.urls')),
 ]
