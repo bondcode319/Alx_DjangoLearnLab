9fb5e00b4a814a4ad144e98c70a69964fca436d5
brianbond <tumuhimbise92@gmail.com>
2025-08-24 21:56:36 +0300

    commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,18 +1,39 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    confirm_password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    token = serializers.SerializerMethodField()
 
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture', 'followers')
-        read_only_fields = ('followers',)
+        fields = ('id', 'username', 'email', 'password', 'confirm_password', 
+                 'bio', 'profile_picture', 'followers', 'token')
+        read_only_fields = ('followers', 'token')
+
+    def get_token(self, obj):
+        token, created = Token.objects.get_or_create(user=obj)
+        return token.key
+
+    def validate(self, data):
+        if data.get('password') != data.get('confirm_password'):
+            raise serializers.ValidationError("Passwords don't match")
+        return data
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        validated_data.pop('confirm_password')
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
         )
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(style={'input_type': 'password'})
